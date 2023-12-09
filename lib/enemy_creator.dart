8501b626fcf9604fa55fc53f1c9385d0f3b9650c
Tacio Medeiros <tacio.medeiros@vitta.me>
2023-12-09 13:51:47 -0300

    fix: corrigindo geracao de objetos

--- lib/enemy_creator.dart
+++ lib/enemy_creator.dart
@@ -9,25 +9,47 @@ class EnemyCreator extends TimerComponent with HasGameRef {
   final Vector2 worldSize;
 
   int stageFactor = 1;
-  int blueToCreate = 1;
   int redAmountForCreation = 1;
+  int blueAmountForCreation = 1;
+  double blueAtack = 10;
+  double redAtack = 10;
 
   //final _halfWidth = Red.initialSize.x / 2;
 
   EnemyCreator(this.worldSize) : super(period: 2, repeat: true);
 
-  @override
-  void onTick() {
+  void addBlueEnemies() {
     gameRef.addAll(
       List.generate(
-        1,
+        blueAmountForCreation,
         (index) => RedEnemy(
           Vector2(
             (random.nextInt(worldSize.x.toInt()) - tileSize).toDouble(),
             (random.nextInt(worldSize.y.toInt()) - tileSize).toDouble(),
           ),
+          blueAtack,
         ),
       ),
     );
+  }
+
+  void addRedEnemies() {
+    gameRef.addAll(
+      List.generate(
+        redAmountForCreation,
+        (index) => RedEnemy(
+          Vector2(
+            (random.nextInt(worldSize.x.toInt()) - tileSize).toDouble(),
+            (random.nextInt(worldSize.y.toInt()) - tileSize).toDouble(),
+          ),
+          redAtack,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void onTick() {
+    addRedEnemies();
   }
 }
--- lib/my_world.dart
+++ lib/my_world.dart
@@ -1,7 +1,6 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:thermostate_wars/config.dart';
 import 'package:thermostate_wars/enemy_creator.dart';
-import 'package:thermostate_wars/red_enemy.dart';
 import 'package:thermostate_wars/wall.dart';
 
 class MyWorld extends WorldMapByTiled {
@@ -9,8 +8,6 @@ class MyWorld extends WorldMapByTiled {
 
   MyWorld(super.path)
       : super(forceTileSize: Vector2(16, 16), objectsBuilder: {
-          'fire-enemy': (TiledObjectProperties properties) =>
-              RedEnemy(properties.position),
           'wall': (TiledObjectProperties properties) =>
               Wall(position: properties.position, size: properties.size),
         });
